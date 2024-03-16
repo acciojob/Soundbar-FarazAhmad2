d43cec88898fca4d17f37b38da7f88408a047d19
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-16 22:41:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',() => {
+  const btns = document.querySelectorAll('.btn');
+  const stopButton = document.querySelector('.stop');
+
+  let playingSound = null;
+  btns.forEach(btn => {
+    let soundFile = `./sounds/${btn.textContent}.mp3`;
+    btn.addEventListener('click', () => {
+      playSound(soundFile);
+    });
+  })
+
+  stopButton.addEventListener('click', () => {
+    stopAllSound();
+  })
+
+  function stopAllSound() {
+    if (playingSound != null) {
+      playingSound.pause();
+      playingSound = null;
+    }
+  }
+
+  function playSound(soundFile) {
+    stopAllSound();
+    sound = new Audio (soundFile);
+    playingSound = sound;
+    playingSound.play();
+  }
+})
